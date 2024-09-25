9aefb89a12e08c16956d6f23dbd863368b467bd6
huncozyboy <leejh5838@naver.com>
2024-09-25 18:35:38 +0900

    first commit

---
+++ src/test/java/land/leets/Carrot/CarrotApplicationTests.java
@@ -0,0 +1,13 @@
+package land.leets.Carrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
