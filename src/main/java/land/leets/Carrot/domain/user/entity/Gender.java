79888ac6a38aab26eb3c4bd4ff1a4d283cdb5cf1
Choi Min Young <codingmy@naver.com>
2024-10-08 12:55:21 +0900

    feature: User erd 변경사항 반영

---
+++ src/main/java/land/leets/Carrot/domain/user/entity/Gender.java
@@ -0,0 +1,5 @@
+package land.leets.Carrot.domain.user.entity;
+
+public enum Gender {
+    MALE, FEMALE
+}
--- src/main/java/land/leets/Carrot/domain/user/entity/User.java
+++ src/main/java/land/leets/Carrot/domain/user/entity/User.java
@@ -4,11 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.sql.Timestamp;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.sql.Timestamp;
 
 @Getter
 @Entity
@@ -17,6 +16,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    private String email;
+
+    private String pw;
+
+    private String phoneNumber;
+
+    private Gender gender;
+
+    private int birthYear;
 
     private String nickname;
 
@@ -29,4 +38,16 @@ public class User {
     private Timestamp lastAccessTime;
 
     private String selfIntroduction;
+
+    private boolean isSmoke = false;
+
+    private boolean isLongWork = false;
+
+    private boolean isCarLicence = false;
+
+    private boolean isEnglish = false;
+
+    private boolean isMilitary = false;
+
+    private boolean isCookLicence = false;
 }
