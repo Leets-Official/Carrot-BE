876d6643ec15b72465735318ad28a73cb577fcfa
huncozyboy <leejh5838@naver.com>
2024-10-10 04:28:24 +0900

    feat #3 jwt 토큰 인증 구현

--- src/main/java/land/leets/Carrot/global/auth/jwt/dto/JwtFilter.java
+++ src/main/java/land/leets/Carrot/global/auth/jwt/dto/JwtFilter.java
@@ -1,4 +1,50 @@
 package land.leets.Carrot.global.auth.jwt.dto;
 
-public class JwtFilter {
+import static land.leets.Carrot.global.auth.exception.ErrorMessage.INVALID_TOKEN;
+
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = getToken(request); // 요청헤더에서 jwt 추출함
+
+        try {
+            if (token != null) { // 추출한 토큰이 null이 아니면
+                jwtProvider.validateToken(token); // 유효성 검증
+                Authentication authentication = jwtProvider.getAuthentication(token); //authentication 객체 가져오기
+                SecurityContextHolder.getContext().setAuthentication(authentication); // SecurityContextHolder에 설정함
+            }
+        } catch (JwtException e) { // 유효성 검증 실패시 JwtException 발생
+            log.info("error token: {}", e.getMessage()); // 로그 메세지 기록
+            request.setAttribute("jwtException", INVALID_TOKEN.getCode()); // 유효하지 않은 토큰 401
+        }
+        filterChain.doFilter(request, response); //request, response 호출함
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (StringUtils.hasText(token) && token.startsWith("Bearer ")) {
+            // "Bearer "를 뺀 부분만 반환
+            return token.substring(7);
+        }
+        return null;
+    }
 }
--- src/main/java/land/leets/Carrot/global/auth/jwt/dto/JwtProvider.java
+++ src/main/java/land/leets/Carrot/global/auth/jwt/dto/JwtProvider.java
@@ -1,4 +1,84 @@
 package land.leets.Carrot.global.auth.jwt.dto;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import jakarta.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.Date;
+import javax.crypto.spec.SecretKeySpec;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class JwtProvider {
+    @Value("${carrot.jwt.key}") // 환경변수 JWT_KEY
+    private String jwtSecret; // JWT 서명 키
+
+    @Value("${carrot.jwt.access.expiration}") // 환경변수 ACCESS_EXP
+    private long accessTokenExpirationTime; // 액세스 토큰 유효기간
+
+    private SecretKeySpec key; // JWT 키
+
+    @PostConstruct
+    public void init() {
+        // jwtSecret 주입된 후 SecretKeySpec 생성하여 초기화함
+        this.key = new SecretKeySpec(jwtSecret.getBytes(), SignatureAlgorithm.HS256.getJcaName());
+    }
+
+    // 액세스 토큰 생성 메소드
+    public String generateAccessToken(String email) { // 이메일 기반으로 jwt 생성함
+        final Date now = new Date();
+        final Date expiration = new Date(now.getTime() + accessTokenExpirationTime * 1000); // 액세스 토큰 유효기간 밀리초로 변환
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(key)
+                .compact();
+    }
+
+    // 토큰 검증 메소드
+    public void validateToken(String token) { // 토큰 검증
+        try {
+            parseToken(token);
+        } catch (ExpiredJwtException e) {
+            log.warn("JwtProvider: Expired token");
+            throw e;
+        } catch (UnsupportedJwtException e) {
+            log.warn("JwtProvider: Unsupported token");
+            throw e;
+        } catch (MalformedJwtException e) {
+            log.warn("JwtProvider: Malformed token");
+            throw e;
+        } catch (IllegalArgumentException e) {
+            log.warn("JwtProvider: IllegalArgumentException");
+            throw e;
+        }
+    }
+
+    private Claims parseToken(String token) {  // Claims(jwt에 저장된 사용자 정보를 포함)를 가져옴
+        return Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    // 인증 정보를 가져오는 메소드
+    public Authentication getAuthentication(String token) {
+        Claims claims = parseToken(token);
+        String email = claims.getSubject(); // 이메일 추출
+
+        // 이메일로 UsernamePasswordAuthenticationToken 반환하여 인증 정보를 생성함
+        return new UsernamePasswordAuthenticationToken(email, null, Collections.emptyList());
+    }
 }
