8f4fa6a230c88794d57c485e133d581ce8c7cd0c
huncozyboy <leejh5838@naver.com>
2024-10-09 17:03:50 +0900

    fix: test 코드 오류 해결

--- src/test/java/land/leets/Carrot/CarrotApplicationTests.java
+++ src/test/java/land/leets/Carrot/CarrotApplicationTests.java
@@ -3,11 +3,11 @@ package land.leets.Carrot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CarrotApplication.class)
 class CarrotApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
