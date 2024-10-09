5307576e8ea6a1c768f54a2d3f267eb9c8f0b71e
Choi Min Young <codingmy@naver.com>
2024-10-09 11:58:28 +0900

    fix: 추상클래스 삭제

--- src/main/java/land/leets/Carrot/domain/location/entity/DetailArea.java
+++ src/main/java/land/leets/Carrot/domain/location/entity/DetailArea.java
@@ -1,7 +1,18 @@
 package land.leets.Carrot.domain.location.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
-public class DetailArea extends AreaBaseEntity {
+@Getter
+public class DetailArea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int areaId;
+
+    private String name;
+
 }
