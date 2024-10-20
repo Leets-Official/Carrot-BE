8529c619812d68f2f98b290373744296bbc31bdf
huncozyboy <leejh5838@naver.com>
2024-10-20 20:28:52 +0900

    feat #5 로그인, 회원가입 dto Validation 추가

--- src/main/java/land/leets/Carrot/domain/user/dto/request/LoginRequest.java
+++ src/main/java/land/leets/Carrot/domain/user/dto/request/LoginRequest.java
@@ -1,11 +1,14 @@
 package land.leets.Carrot.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class LoginRequest {
+    @NotNull
     private String email;
+    @NotNull
     private String password;
 }
--- src/main/java/land/leets/Carrot/domain/user/dto/request/UserSignupRequest.java
+++ src/main/java/land/leets/Carrot/domain/user/dto/request/UserSignupRequest.java
@@ -1,13 +1,18 @@
 package land.leets.Carrot.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class UserSignupRequest {
+    @NotNull
     private String email;
+    @NotNull
     private String password;
+    @NotNull
     private String phoneNumber;
+    @NotNull
     private String nickname;
 }
