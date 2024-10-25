c775f537dfac530def9256f7aba29f9522716621
huncozyboy <leejh5838@naver.com>
2024-10-25 18:20:39 +0900

    feat #7 고용자 회원가입 관련 예외 메세지 생성

--- src/main/java/land/leets/Carrot/domain/user/exception/ErrorMessage.java
+++ src/main/java/land/leets/Carrot/domain/user/exception/ErrorMessage.java
@@ -11,6 +11,9 @@ public enum ErrorMessage {
     EMAIL_ALREADY_EXISTS(400, "이미 존재하는 이메일입니다."),
     TEL_ALREADY_EXISTS(400, "이미 등록된 전화번호입니다."),
     NICKNAME_ALREADY_EXISTS(400, "이미 존재하는 닉네임입니다."),
+    CEONUMBER_ALREADY_EXISTS(400, "이미 존재하는 사업자 번호입니다"),
+    CEONAME_ALREADY_EXISTS(400, "이미 존재하는 사업자 이름입니다."),
+
     // 로그인 관련
     USER_NOT_FOUND(404, "존재하지 않는 유저입니다."),
     INVALID_PASSWORD(401, "비밀번호가 일치하지 않습니다.");
