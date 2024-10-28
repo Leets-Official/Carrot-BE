5ab42833c077646a62627beed56be45e83ac0bf2
Choi Min Young <codingmy@naver.com>
2024-10-29 00:16:17 +0900

    feat #16 지역정보 엔티티 DetailArea 하나로 통합

--- src/main/java/land/leets/Carrot/domain/location/entity/DetailArea.java
+++ src/main/java/land/leets/Carrot/domain/location/entity/DetailArea.java
@@ -4,15 +4,25 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+//도,시,동 엔티티 테이블 하나로 합치는 방향으로 설계 변경
 
 @Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DetailArea {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int areaId;
+    private Integer areaId;
 
     private String name;
+    @Builder
+    public DetailArea(String doName) {
+        this.name = doName;
+    }
 
 }
