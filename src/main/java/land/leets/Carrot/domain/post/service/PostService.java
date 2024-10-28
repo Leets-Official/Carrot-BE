595b45b0dd6d94723f5aebeb4fe2bff667613b88
Choi Min Young <codingmy@naver.com>
2024-10-29 00:17:48 +0900

    feat #16 PostService에 지역 데이터 변환 로직 추가

--- src/main/java/land/leets/Carrot/domain/post/service/PostService.java
+++ src/main/java/land/leets/Carrot/domain/post/service/PostService.java
@@ -1,25 +1,39 @@
 package land.leets.Carrot.domain.post.service;
 
 import java.time.LocalDateTime;
-import land.leets.Carrot.domain.post.dto.response.PostResponse;
-import land.leets.Carrot.domain.post.entity.PostSnapshot;
-import land.leets.Carrot.domain.post.repository.PostRepository;
-import land.leets.Carrot.domain.post.dto.request.PostPostRequest;
-import land.leets.Carrot.domain.post.entity.Post;
+import land.leets.Carrot.domain.location.entity.DetailArea;
+import land.leets.Carrot.domain.location.repository.LocationRepository;
 import land.leets.Carrot.domain.post.domain.PostData;
+import land.leets.Carrot.domain.post.dto.request.PostPostRequest;
+import land.leets.Carrot.domain.post.dto.response.PostResponse;
+import land.leets.Carrot.domain.post.entity.Post;
+import land.leets.Carrot.domain.post.entity.PostSnapshot;
+import land.leets.Carrot.domain.post.mapper.PostDataMapper;
+import land.leets.Carrot.domain.post.mapper.PostSnapshotMapper;
+import land.leets.Carrot.domain.post.repository.PostRepository;
 import land.leets.Carrot.domain.post.repository.PostSnapshotRepository;
 import land.leets.Carrot.global.common.exception.BaseException;
 
 public class PostService {
     PostRepository postRepository;
     PostSnapshotRepository postSnapshotRepository;
+    LocationRepository locationRepository;
+
     public void saveNewPost(PostPostRequest postPostRequest) {
         PostData postData = postPostRequest.postData();
-        PostSnapshot postSnapshot = new PostSnapshot();
+        Integer doAreaId = getAreaId(postData.doName());
+        Integer siAreaId = getAreaId(postData.siName());
+        Integer detailAreaId = getAreaId(postData.detailName());
 
         Post post = new Post(postPostRequest.userId(), postPostRequest.storeName(), LocalDateTime.now(), "Recruting");
         postRepository.save(post);
         postSnapshotRepository.save(postSnapshot);
+    }
+
+    public Integer getAreaId(String areaName) {
+        return locationRepository.findByName(areaName)
+                .orElse(locationRepository.save(new DetailArea(areaName)))
+                .getAreaId();
     }
 
     public PostResponse getPost(Long postId){
