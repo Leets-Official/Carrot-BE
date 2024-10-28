fb14a0da2fc66d4260c042baa151703f1c0d99db
Choi Min Young <codingmy@naver.com>
2024-10-28 22:15:05 +0900

    feat #16 게시글 상세 조회 repository 메서드 추가

--- src/main/java/land/leets/Carrot/domain/post/repository/PostSnapshotRepository.java
+++ src/main/java/land/leets/Carrot/domain/post/repository/PostSnapshotRepository.java
@@ -1,7 +1,12 @@
 package land.leets.Carrot.domain.post.repository;
 
+import java.util.Optional;
 import land.leets.Carrot.domain.post.entity.PostSnapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostSnapshotRepository extends JpaRepository<PostSnapshot, Long> {
+    @Override
+    Optional<PostSnapshot> findById(Long aLong);
+
+    PostSnapshot findByPostIdAndLastestTrue(Long postId);
 }
