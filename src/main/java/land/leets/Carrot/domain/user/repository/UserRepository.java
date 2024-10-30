39bd09634b056482e58919d175567e0b25269b11
huncozyboy <leejh5838@naver.com>
2024-10-31 06:31:48 +0900

    feat #17 User 레포에 email로 접근하는 메서드 구현

--- src/main/java/land/leets/Carrot/domain/user/repository/UserRepository.java
+++ src/main/java/land/leets/Carrot/domain/user/repository/UserRepository.java
@@ -5,15 +5,5 @@ import land.leets.Carrot.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-
-    boolean existsByPhoneNumber(String phoneNumber);
-
-    boolean existsByNickname(String nickname);
-
-    boolean existsByCeoNumber(String ceoNumber);
-
-    boolean existsByCeoName(String ceoName);
-
     Optional<User> findByEmail(String email);
 }
