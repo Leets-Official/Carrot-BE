e4ae404ed48e05cefadb199354a56e4fa5902dd5
huncozyboy <leejh5838@naver.com>
2024-10-31 07:11:13 +0900

    feat #17 프로필 관련 성공 메세지 추가

--- src/main/java/land/leets/Carrot/domain/user/controller/ResponseMessage.java
+++ src/main/java/land/leets/Carrot/domain/user/controller/ResponseMessage.java
@@ -7,9 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseMessage {
 
-
+    //로그인 회원가입 관련
     USER_SAVE_SUCCESS(201, "회원가입 성공."),
-    LOGIN_SUCCESS(200, "로그인 성공.");
+    LOGIN_SUCCESS(200, "로그인 성공."),
+    //마이페이지(프로필) 관련
+    PROFILE_CHECK_SUCCESS(200, "프로필 조회 성공."),
+    PROFILE_UPDATED_SUCCESS(200, "프로필 수정 성공.");
 
     private final int code;
     private final String message;
