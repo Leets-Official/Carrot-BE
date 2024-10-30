4ea575838e67708d122344dd259fd8ccaf6e617d
huncozyboy <leejh5838@naver.com>
2024-10-30 19:13:09 +0900

    refactor #18 수정된 common 예외처리 맞추어 user 예외처리 변경

--- src/main/java/land/leets/Carrot/domain/user/exception/UserAlreadyExistsException.java
+++ src/main/java/land/leets/Carrot/domain/user/exception/UserAlreadyExistsException.java
@@ -1,9 +1,11 @@
 package land.leets.Carrot.domain.user.exception;
 
+import static land.leets.Carrot.domain.user.exception.ErrorMessage.USER_ALREADY_EXISTS;
+
 import land.leets.Carrot.global.common.exception.BaseException;
 
 public class UserAlreadyExistsException extends BaseException {
-    public UserAlreadyExistsException(ErrorMessage errorMessage) {
-        super(errorMessage.getCode(), errorMessage.getMessage());
+    public UserAlreadyExistsException() {
+        super(USER_ALREADY_EXISTS.getCode(), USER_ALREADY_EXISTS.getMessage());
     }
 }
