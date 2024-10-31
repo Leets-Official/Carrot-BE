0e2a7dd06fe8470ed8f1dbb514ddc8ce3004374c
Choi Min Young <codingmy@naver.com>
2024-11-01 01:40:55 +0900

    feat #16 status 값으로 조회하는 repository 메서드 생성

--- src/main/java/land/leets/Carrot/domain/post/repository/PostRepository.java
+++ src/main/java/land/leets/Carrot/domain/post/repository/PostRepository.java
@@ -1,11 +1,16 @@
 package land.leets.Carrot.domain.post.repository;
 
+import java.util.List;
 import java.util.Optional;
 import land.leets.Carrot.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     @Override
     Optional<Post> findById(Long aLong);
 
+    @Query("SELECT post FROM Post post WHERE post.status=:recuiting")
+    Optional<List<Post>> findByStatus(@Param("status") String status);
 }
