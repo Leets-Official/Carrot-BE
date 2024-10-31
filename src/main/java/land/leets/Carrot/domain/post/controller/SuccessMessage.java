485ae0367d998c08dbd8fd13ce8a01fe4c196e76
Choi Min Young <codingmy@naver.com>
2024-11-01 02:46:21 +0900

    feat #16 특정 유저 본인이 작성한 공고글 조회 dto, 도메인 생성

--- src/main/java/land/leets/Carrot/domain/post/controller/SuccessMessage.java
+++ src/main/java/land/leets/Carrot/domain/post/controller/SuccessMessage.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SuccessMessage {
 
-    GET_POST_DETAIL_SUCCESS(200, "세부 게시글 데이터 조회 성공.");
+    GET_POST_DETAIL_SUCCESS(200, "세부 게시글 데이터 조회 성공."),
+    GET_POSTED_POST_LIST_SUCCESS(200, "유저가 작성한 게시글 조회 성공");
 
     private final int code;
     private final String message;
