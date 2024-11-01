a52f6ec4ea9e7c859d94871330dfb174299523ba
Choi Min Young <codingmy@naver.com>
2024-11-01 15:04:41 +0900

    feat #16 키워드로 검색 repository 메서드 구현

--- src/main/java/land/leets/Carrot/domain/post/repository/PostSnapshotRepository.java
+++ src/main/java/land/leets/Carrot/domain/post/repository/PostSnapshotRepository.java
@@ -1,12 +1,19 @@
 package land.leets.Carrot.domain.post.repository;
 
+import java.util.List;
 import java.util.Optional;
 import land.leets.Carrot.domain.post.entity.PostSnapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostSnapshotRepository extends JpaRepository<PostSnapshot, Long> {
     @Override
     Optional<PostSnapshot> findById(Long aLong);
 
     Optional<PostSnapshot> findByPostIdAndLastestTrue(Long postId);
+
+    @Query("SELECT postsnapshot from PostSnapshot postsnapshot WHERE postsnapshot.title LIKE %:keyword%")
+    Optional<List<PostSnapshot>> findByKeywordAndLastestTrue(@Param("keyword") String keyword);
+
 }
