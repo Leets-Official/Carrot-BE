0abade13404dc4ff2badfa89fd1195b6016416b0
huncozyboy <leejh5838@naver.com>
2024-11-01 18:31:52 +0900

    refactor #21 동명이인이 있을 수 있어서 불필요한 예외처리 삭제

--- src/main/java/land/leets/Carrot/domain/user/repository/EmployeeRepository.java
+++ src/main/java/land/leets/Carrot/domain/user/repository/EmployeeRepository.java
@@ -7,6 +7,5 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     boolean existsByEmail(String email);
 
     boolean existsByPhoneNumber(String phoneNumber);
-
-    boolean existsByNickname(String nickname);
+    
 }
