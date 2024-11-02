b05d39e3e13b386f98b84585b15c51ede481140d
Choi Min Young <codingmy@naver.com>
2024-11-02 20:29:51 +0900

    feat #25 response dto 생성

---
+++ src/main/java/land/leets/Carrot/domain/apply/domain/Applicant.java
@@ -0,0 +1,8 @@
+package land.leets.Carrot.domain.apply.domain;
+
+public record Applicant(
+        Long userId,
+        String userNickname,
+        String detailAreaName
+) {
+}
---
+++ src/main/java/land/leets/Carrot/domain/apply/dto/response/GetApplicantResponse.java
@@ -0,0 +1,8 @@
+package land.leets.Carrot.domain.apply.dto.response;
+
+import java.util.List;
+import land.leets.Carrot.domain.apply.domain.Applicant;
+
+public class GetApplicantResponse {
+    List<Applicant> applicantList;
+}
