c76c5b4cc06c077a6e4688577dd46a55a6a5d4ee
Choi Min Young <codingmy@naver.com>
2024-11-02 20:06:35 +0900

    feat #25 지원자를 고용된 유저로 상태 변경 repository 메서드 추가

--- src/main/java/land/leets/Carrot/domain/apply/repository/ApplyRepository.java
+++ src/main/java/land/leets/Carrot/domain/apply/repository/ApplyRepository.java
@@ -1,7 +1,13 @@
 package land.leets.Carrot.domain.apply.repository;
 
+import java.util.Optional;
 import land.leets.Carrot.domain.apply.entity.Apply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
+
+    @Query("SELECT apply FROM Apply apply where apply.postId = :postId AND apply.userId = :userId")
+    Optional<Apply> findByPostIdAndUserID (@Param("postId") Long postId, @Param("userId") Long userId);
 }
