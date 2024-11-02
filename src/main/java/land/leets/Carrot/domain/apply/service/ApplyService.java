309d062fe8bee02348745550214ebc6cde5e6f1d
Choi Min Young <codingmy@naver.com>
2024-11-02 20:12:01 +0900

    feat #25 게시글 지원 api 예외처리 추가

--- src/main/java/land/leets/Carrot/domain/apply/service/ApplyService.java
+++ src/main/java/land/leets/Carrot/domain/apply/service/ApplyService.java
@@ -1,12 +1,14 @@
 package land.leets.Carrot.domain.apply.service;
 
 import static land.leets.Carrot.domain.apply.exception.ApplyErrorMessage.APPLY_NOT_FOUND;
+import static land.leets.Carrot.domain.apply.exception.ApplyErrorMessage.POST_NOT_RECRUITING;
 
 import jakarta.transaction.Transactional;
 import land.leets.Carrot.domain.apply.dto.request.PostApplyRequest;
 import land.leets.Carrot.domain.apply.entity.Apply;
 import land.leets.Carrot.domain.apply.exception.ApplyException;
 import land.leets.Carrot.domain.apply.repository.ApplyRepository;
+import land.leets.Carrot.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +17,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ApplyService {
     private final ApplyRepository applyRepository;
+    private final PostRepository postRepository;
+
+    private static final String POST_STATUS_RECRUITING = "recruiting";
 
     public void postApply(PostApplyRequest postApplyRequest) {
         Apply apply = new Apply(postApplyRequest.postId(), postApplyRequest.userId());
-        applyRepository.save(apply);
+        if (postRepository.findById(postApplyRequest.postId()).equals(POST_STATUS_RECRUITING)) {
+            applyRepository.save(apply);
+        } else {
+            throw new ApplyException(POST_NOT_RECRUITING);
+        }
     }
 
     public void setStatusRecruited(PostApplyRequest postApplyRequest) {
