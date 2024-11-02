9cab33b025edafc0f20cf4d6bea029ed137ddd0f
Choi Min Young <codingmy@naver.com>
2024-11-03 00:13:43 +0900

    feat #25 지원자 조회 response dto 생성

--- src/main/java/land/leets/Carrot/domain/apply/dto/response/GetApplicantResponse.java
+++ src/main/java/land/leets/Carrot/domain/apply/dto/response/GetApplicantResponse.java
@@ -3,6 +3,7 @@ package land.leets.Carrot.domain.apply.dto.response;
 import java.util.List;
 import land.leets.Carrot.domain.apply.domain.Applicant;
 
-public class GetApplicantResponse {
-    List<Applicant> applicantList;
+public record GetApplicantResponse(
+        List<Applicant> applicantList
+) {
 }
