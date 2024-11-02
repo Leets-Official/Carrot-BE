d08356441d99a0bc704711a72607c1a7a153a192
Choi Min Young <codingmy@naver.com>
2024-11-03 02:21:21 +0900

    feat #25 중복 지원시 예외처리 구현

--- src/main/java/land/leets/Carrot/domain/apply/exception/ApplyErrorMessage.java
+++ src/main/java/land/leets/Carrot/domain/apply/exception/ApplyErrorMessage.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ApplyErrorMessage {
+    ALREADY_APPLIED(HttpStatus.BAD_REQUEST.value(), "이미 지원한 게시글입니다"),
+
     APPLY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 유저의 이 게시글의 지원 내역이 없습니다."),
     POST_NOT_RECRUITING(HttpStatus.NOT_FOUND.value(), "해당 게시글은 구인 중이 아닙니다."),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 게시글을 찾을 수 없습니다."),
