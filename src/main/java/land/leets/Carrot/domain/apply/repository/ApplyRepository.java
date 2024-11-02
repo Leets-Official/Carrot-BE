1d76955af91b4e71fe7e19fa44ada4b94bb4d5ad
Choi Min Young <codingmy@naver.com>
2024-11-02 20:30:28 +0900

    feat #25 게시글의 지원자id 조회 repository 메서드 생성

--- src/main/java/land/leets/Carrot/domain/apply/repository/ApplyRepository.java
+++ src/main/java/land/leets/Carrot/domain/apply/repository/ApplyRepository.java
@@ -1,5 +1,6 @@
 package land.leets.Carrot.domain.apply.repository;
 
+import java.util.List;
 import java.util.Optional;
 import land.leets.Carrot.domain.apply.entity.Apply;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,7 @@ public interface ApplyRepository extends JpaRepository<Apply, Long> {
 
     @Query("SELECT apply FROM Apply apply where apply.postId = :postId AND apply.userId = :userId")
     Optional<Apply> findByPostIdAndUserID (@Param("postId") Long postId, @Param("userId") Long userId);
+
+    @Query("SELECT apply FROM Apply apply where apply.postId =:postId")
+    List<Apply> findByPostId(@Param("postId") Long postId);
 }
