041a24bf338b0471f60b378888cb0fe86bfcd73d
huncozyboy <leejh5838@naver.com>
2024-11-03 19:56:19 +0900

    refactor #23 불필요한 성공 메세지 삭제

--- src/main/java/land/leets/Carrot/domain/user/controller/ResponseMessage.java
+++ src/main/java/land/leets/Carrot/domain/user/controller/ResponseMessage.java
@@ -12,7 +12,6 @@ public enum ResponseMessage {
     LOGIN_SUCCESS(200, "로그인 성공."),
     //마이페이지(프로필) 관련
     PROFILE_CHECK_SUCCESS(200, "프로필 조회 성공."),
-    PROFILE_UPDATED_SUCCESS(200, "프로필 수정 성공."),
     BASIC_INFO_UPDATE_SUCCESS(200, "프로필 기본정보 수정 성공"),
     CAREER_UPDATE_SUCCESS(200, "경력 정보 수정 성공"),
     SELF_INTRO_UPDATE_SUCCESS(200, "자기소개 정보 수정 성공"),
