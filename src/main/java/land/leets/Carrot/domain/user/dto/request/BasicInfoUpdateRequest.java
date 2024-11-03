26400534acf4c47f0826d3473e7426f80f122354
huncozyboy <leejh5838@naver.com>
2024-11-03 19:59:37 +0900

    refactor #23 userId 입력부분 수정

--- src/main/java/land/leets/Carrot/domain/user/dto/request/BasicInfoUpdateRequest.java
+++ src/main/java/land/leets/Carrot/domain/user/dto/request/BasicInfoUpdateRequest.java
@@ -1,6 +1,5 @@
 package land.leets.Carrot.domain.user.dto.request;
 
-import jakarta.validation.constraints.NotNull;
 import land.leets.Carrot.domain.user.entity.Gender;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,9 +7,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class BasicInfoUpdateRequest {
-    @NotNull
-    private Long userId;
-
     // 공통
     private Gender gender;
     private Integer birthYear;
