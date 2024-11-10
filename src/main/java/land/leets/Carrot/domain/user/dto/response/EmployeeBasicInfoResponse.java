42a3519c5c713b51a5a50a6e10c9e99c0fc4b431
huncozyboy <leejh5838@naver.com>
2024-11-10 22:16:24 +0900

    refactor #56 구직자 기본정보 수정 응답 dto Record로 수정

---
+++ src/main/java/land/leets/Carrot/domain/user/dto/response/EmployeeBasicInfoResponse.java
@@ -0,0 +1,22 @@
+package land.leets.Carrot.domain.user.dto.response;
+
+import land.leets.Carrot.domain.user.entity.Employee;
+import land.leets.Carrot.domain.user.entity.Gender;
+
+public record EmployeeBasicInfoResponse(
+        String employeeName,
+        String employeeAddress,
+        String phoneNumber,
+        Gender gender,
+        Integer birthYear
+) implements UserBasicInfoResponse {
+    public static EmployeeBasicInfoResponse from(Employee employee) {
+        return new EmployeeBasicInfoResponse(
+                employee.getEmployeeName(),
+                employee.getEmployeeAddress(),
+                employee.getPhoneNumber(),
+                employee.getGender(),
+                employee.getBirthYear()
+        );
+    }
+}
