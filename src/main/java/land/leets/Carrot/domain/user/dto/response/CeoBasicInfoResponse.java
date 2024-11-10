ae016dee5ab0d316487da5fabb6a8ed0a2a08dfa
huncozyboy <leejh5838@naver.com>
2024-11-10 22:15:40 +0900

    refactor #56 프로필 기본정보 응답 Dto로 이름 변경

--- src/main/java/land/leets/Carrot/domain/user/dto/response/CeoBasicInfoResponse.java
+++ src/main/java/land/leets/Carrot/domain/user/dto/response/CeoBasicInfoResponse.java
@@ -3,15 +3,15 @@ package land.leets.Carrot.domain.user.dto.response;
 import land.leets.Carrot.domain.user.entity.Ceo;
 import land.leets.Carrot.domain.user.entity.Gender;
 
-public record CeoProfileResponse(
+public record CeoBasicInfoResponse(
         String ceoName,
         String ceoPhoneNumber,
         String ceoAddress,
         Gender gender,
         Integer birthYear
-) implements ProfileResponse {
-    public static CeoProfileResponse from(Ceo ceo) {
-        return new CeoProfileResponse(
+) implements UserBasicInfoResponse {
+    public static CeoBasicInfoResponse from(Ceo ceo) {
+        return new CeoBasicInfoResponse(
                 ceo.getCeoName(),
                 ceo.getCeoPhoneNumber(),
                 ceo.getCeoAddress(),
