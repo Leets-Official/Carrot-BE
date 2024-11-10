b8d9b8cf2d82ef7d2df06ba475db80ed0264dc2f
huncozyboy <leejh5838@naver.com>
2024-11-10 22:17:05 +0900

    refactor #56 구직자 메인페이지(내 지원서 관리) 응답 dto 수정

--- src/main/java/land/leets/Carrot/domain/user/dto/response/EmployeeProfileResponse.java
+++ src/main/java/land/leets/Carrot/domain/user/dto/response/EmployeeProfileResponse.java
@@ -4,19 +4,53 @@ import land.leets.Carrot.domain.user.entity.Employee;
 import land.leets.Carrot.domain.user.entity.Gender;
 
 public record EmployeeProfileResponse(
+        String phoneNumber,
         String employeeName,
         String employeeAddress,
-        String phoneNumber,
+        String workplace,
+        String workType,
+        String workYear,
+        String workPeriod,
+        String selfIntro,
+        boolean isSmoke,
+        boolean isLongWork,
+        boolean isCarLicense,
+        boolean isEnglish,
+        boolean isMilitary,
+        boolean isCookLicense,
+        boolean isDiligent,
+        boolean isOnTime,
+        boolean isClean,
+        boolean isNearHome,
+        boolean isSleepless,
         Gender gender,
-        Integer birthYear
-) implements ProfileResponse {
+        Integer birthYear,
+        String profileImageUrl
+) {
     public static EmployeeProfileResponse from(Employee employee) {
         return new EmployeeProfileResponse(
+                employee.getPhoneNumber(),
                 employee.getEmployeeName(),
                 employee.getEmployeeAddress(),
-                employee.getPhoneNumber(),
+                employee.getWorkplace(),
+                employee.getWorkType(),
+                employee.getWorkYear(),
+                employee.getWorkPeriod(),
+                employee.getSelfIntro(),
+                employee.isSmoke(),
+                employee.isLongWork(),
+                employee.isCarLicense(),
+                employee.isEnglish(),
+                employee.isMilitary(),
+                employee.isCookLicense(),
+                employee.isDiligent(),
+                employee.isOnTime(),
+                employee.isClean(),
+                employee.isNearHome(),
+                employee.isSleepless(),
                 employee.getGender(),
-                employee.getBirthYear()
+                employee.getBirthYear(),
+                employee.getProfileImageUrl()
         );
     }
 }
